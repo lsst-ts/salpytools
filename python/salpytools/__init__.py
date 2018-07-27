d73f5f364251b63c98063e32090ba82553e37bb1
Tiago Ribeiro <tiago.astro@gmail.com>
2018-07-27 12:24:25 -0700

    Remove Context from `__init__`.

--- python/salpytools/__init__.py
+++ python/salpytools/__init__.py
@@ -2,7 +2,6 @@ __author__  = "LSST/Felipe Menanteau"
 __version__ = '0.1.0'
 version = __version__
 
-from .salpylib import Context
 from .salpylib import DDSController
 from .salpylib import DDSSubscriber
 from .salpylib import DDSSend
