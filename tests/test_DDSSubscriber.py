7d7ceae90074629d667b3b5ace35ed6bf18619d5
Andrew Heyer <aheyer@lsst.org>
2018-08-31 11:21:10 -0700

    test fixes

--- tests/test_DDSSubscriber.py
+++ tests/test_DDSSubscriber.py
@@ -2,12 +2,7 @@ import unittest
 from salpytools import salpylib
 
 
-class DummyHandle():
-	# Object used for passing as a Handle in our tests
-	def __init__(self):
-		self.name = ""
-
-class TestDDSSubscriber(unittest.TestCase):
+class TestDDSSubscriberThread(unittest.TestCase):
 
 	def test_invalid_parse_for_subsystem_tag(self):
 		
@@ -21,24 +16,24 @@ class TestDDSSubscriber(unittest.TestCase):
 		topic7 = "__"
 		topic8 = "_"
 
-		handle = DummyHandle()
+		handle = {}
 
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic1, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic1, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic2, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic2, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic3, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic3, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic4, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic4, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic5, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic5, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic6, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic6, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic7, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic7, handle = handle)
 		with self.assertRaises(ValueError):
-			subscriber = salpylib.DDSSubscriber(topic = topic8, handle = handle)
+			subscriber = salpylib.DDSSubscriberThread(topic = topic8, handle = handle)
 
 	def test_valid_parse_for_subsystem_tag(self):
 		
@@ -47,13 +42,13 @@ class TestDDSSubscriber(unittest.TestCase):
 		topic2 = "subsystemtag_logevent_arbitrarystring"
 		topic3 = "subsystemtag_arbitrarystring"
 
-		handle = DummyHandle()
+		handle = {}
 
-		subscriber1 = salpylib.DDSSubscriber(topic = topic1, handle = handle)
+		subscriber1 = salpylib.DDSSubscriberThread(topic = topic1, handle = handle)
 
-		subscriber2 = salpylib.DDSSubscriber(topic = topic2, handle = handle)
+		subscriber2 = salpylib.DDSSubscriberThread(topic = topic2, handle = handle)
 
-		subscriber3 = salpylib.DDSSubscriber(topic = topic3, handle = handle)
+		subscriber3 = salpylib.DDSSubscriberThread(topic = topic3, handle = handle)
 
 		self.assertEqual(subscriber1.subsystem_tag, "subsystemtag")
 		self.assertEqual(subscriber2.subsystem_tag, "subsystemtag")
@@ -62,24 +57,24 @@ class TestDDSSubscriber(unittest.TestCase):
 	def test_invalid_SALPY_import(self):
 		# Invalid subsytem tag passed to the DDSSubscirber
 		topic = "invalid_subsytemtag"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		with self.assertRaises(ModuleNotFoundError):
 			subscriber.set_salpy_lib()
 
 	def test_valid_SALPY_import(self):
-		# Simple instantiation of a DDSSubscriber
+		# Simple instantiation of a DDSSubscriberThread
 		topic = "scheduler_notneededforthistest"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		self.assertEqual(subscriber.subsystem_tag, "scheduler")
 
 	def test_command_topic(self):
 		# The DDSSubsciber should not handle commands
 		topic = "scheduler_command_enterControl"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		subscriber.set_mgr()
 		with self.assertRaises(ValueError):
@@ -88,8 +83,8 @@ class TestDDSSubscriber(unittest.TestCase):
 	def test_event_topic(self):
 		# Only the flag for the event should be true, the others should be false.
 		topic = "scheduler_logevent_target"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		subscriber.set_mgr()
 		subscriber.mgr_subscribe_to_topic()
@@ -101,8 +96,8 @@ class TestDDSSubscriber(unittest.TestCase):
 	def test_telemtry_topic(self):
 		# Only the flag for the telemetry should be true, the others should be false.
 		topic = "scheduler_bulkCloud"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		subscriber.set_mgr()
 		subscriber.mgr_subscribe_to_topic()
@@ -114,8 +109,8 @@ class TestDDSSubscriber(unittest.TestCase):
 	def test_invalid_set_data(self):
 		# Only the flag for the telemetry should be true, the others should be false.
 		topic = "scheduler_bulkCloud"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		subscriber.set_mgr()
 		# Invalid because we should call subscriber.mgr_subscribe_to_topic()
@@ -125,8 +120,8 @@ class TestDDSSubscriber(unittest.TestCase):
 	def test_valid_set_data(self):
 		# Only the flag for the telemetry should be true, the others should be false.
 		topic = "scheduler_bulkCloud"
-		handle = DummyHandle()
-		subscriber = salpylib.DDSSubscriber(topic = topic, handle = handle)
+		handle = {}
+		subscriber = salpylib.DDSSubscriberThread(topic = topic, handle = handle)
 		subscriber.set_salpy_lib()
 		subscriber.set_mgr()
 		subscriber.mgr_subscribe_to_topic()
