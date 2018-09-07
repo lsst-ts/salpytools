d025d0b27cc1c74ad38c98c5032b053474528048
Lynne Jones <lynnej@uw.edu>
2018-09-07 14:58:17 -0700

    Reformat and extend comments, flake8.

--- python/salpytools/states.py
+++ python/salpytools/states.py
@@ -1,24 +1,39 @@
-'''
+"""
 Definitions for states of SAL compoments.
 Taken from file toolsmod.py in:
 https://github.com/lsst/ctrl_iip/blob/master/python/lsst/iip/toolsmod.py
-'''
+"""
 
 import time
 import logging
 import inspect
-from salpytools.utils import load_SALPYlib
-from salpytools.state_transition_exception import StateTransitionException
+from .utils import load_SALPYlib
+from .state_transition_exception import StateTransitionException
 
-summary_state_enum = {'DISABLE':0,
-                      'ENABLE':1,
-                      'FAULT':2,
-                      'OFFLINE':3,
-                      'STANDBY':4}
+__all__ = ['OfflineState', 'StandbyState', 'DisabledState', 'EnabledState', 'FaultState']
+
+
+summary_state_enum = {'DISABLE': 0,
+                      'ENABLE': 1,
+                      'FAULT': 2,
+                      'OFFLINE': 3,
+                      'STANDBY': 4}
 
 
 class DefaultState:
+    """Base class for states in stateMachine.
+    Loads SALPY_lib
 
+    Parameters
+    ----------
+    name: str
+    subsystem_tag: str
+        Name of the subsystem SALPY library to load.
+    events: tuple of strings, opt
+        Default = ('summaryState',)
+    tsleep: float, opt
+        Default 0.5
+    """
     def __init__(self, name, subsystem_tag, events=('summaryState',), tsleep=0.5):
         self.name = name
         self.subsystem_tag = subsystem_tag
@@ -36,29 +51,31 @@ class DefaultState:
             self.subscribe_logEvent(event)
 
     def subscribe_logEvent(self, eventname):
-        """
-        Create a subscription for the {subsystem_tag}_logevent_{eventnname}
+        """Create a subscription for the {subsystem_tag}_logevent_{eventnname}
         This step need to be done before we call send_logEvent
 
-        :param eventname:
-        :return:
+        Parameters
+        ----------
+        eventname: str
+            Name of the event.
         """
         self.mgr[eventname] = getattr(self.SALPY_lib, 'SAL_{}'.format(self.subsystem_tag))()
         self.mgr[eventname].salEvent("{}_logevent_{}".format(self.subsystem_tag, eventname))
         self.logEvent[eventname] = getattr(self.mgr[eventname], 'logEvent_{}'.format(eventname))
-        self.myData[eventname] = getattr(self.SALPY_lib, '{}_logevent_{}C'.format(self.subsystem_tag, eventname))()
+        self.myData[eventname] = getattr(self.SALPY_lib, '{}_logevent_{}C'.format(self.subsystem_tag,
+                                                                                  eventname))()
 
         self.myData_keys[eventname] = [a[0] for a in inspect.getmembers(self.myData[eventname]) if
                                        not (a[0].startswith('__') and a[0].endswith('__'))]
         self.log.debug('Initializing: {}_logevent_{}'.format(self.subsystem_tag, eventname))
 
     def send_logEvent(self, eventname, **kwargs):
-        """
-        Send logevent for an eventname
+        """Send logevent for an eventname
 
-        :param eventname:
-        :param kwargs:
-        :return:
+        Parameters
+        ----------
+        eventname: str
+            Name of the event.
         """
         # Populate myData object for keys across logevent
         # self.myData[eventname].timestamp = kwargs.pop('timestamp',time.time())
@@ -73,7 +90,7 @@ class DefaultState:
         self.logEvent[eventname](self.myData[eventname], priority)
         self.log.info('Sent sucessfully {} Data Object'.format(eventname))
         for key in self.myData_keys[eventname]:
-            self.log.info('\t{}:{}'.format(key,getattr(self.myData[eventname],key)))
+            self.log.info('\t{}:{}'.format(key, getattr(self.myData[eventname], key)))
         time.sleep(self.tsleep)
         return True
 
@@ -84,7 +101,7 @@ class DefaultState:
         self.log.debug('State waking')
         self.send_logEvent('summaryState')
 
-    #<----- Default State methods corresponding to UML design under here ------>
+    # <----- Default State methods corresponding to UML design under here ------>
 
     def disable(self, model):
         raise StateTransitionException()
@@ -109,7 +126,6 @@ class DefaultState:
 
     def do(self, model):
         self.log.debug("Default: do() not implemented")
-
 
 
 class OfflineState(DefaultState):
@@ -153,6 +169,7 @@ class StandbyState(DefaultState):
     def on_heartbeat(self, model):
         pass
 
+
 class DisabledState(DefaultState):
 
     def __init__(self, subsystem_tag, events=('summaryState',), tsleep=0.5):
@@ -186,6 +203,7 @@ class DisabledState(DefaultState):
     def on_interrupt_process_triggers(self, model):
         pass
 
+
 class EnabledState(DefaultState):
 
     def __init__(self, subsystem_tag, events=('summaryState',), tsleep=0.5):
@@ -213,6 +231,7 @@ class EnabledState(DefaultState):
 
     def on_interrupt_process_triggers(self, model):
         pass
+
 
 class FaultState(DefaultState):
 
